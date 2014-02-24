96f1325262bef44958ac420952818c263c76efbe
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-24 10:46:45 -0600

    Agrego nombres
    Se agrego mi nombre al proyecto

---
+++ Parabola/src/parabola/Parabola.java
@@ -0,0 +1,17 @@
+
+package parabola;
+
+/**
+ *
+ * @autores Ruben Martinez Pacheco
+ */
+public class Parabola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
