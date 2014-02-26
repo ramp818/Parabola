c1e57e5afcbe00cb04f48a92d8e44d639caeee55
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-25 22:11:19 -0600

    Agregaron clases
    Se agregaron las principales clases a utilizar

---
+++ Parabola/src/parabola/Atrapador.java
@@ -0,0 +1,16 @@
+package parabola;
+
+public class Atrapador extends Base{
+    
+        /**
+	 * Metodo constructor que hereda los atributos de la clase <code>Universo</code>.
+	 * @param posX es la <code>posiscion en x</code> del objeto Planeta.
+	 * @param posY es el <code>posiscion en y</code> del objeto Planeta.
+	 * @param image es la <code>imagen</code> del objeto Planeta.
+	 */
+    
+    public Atrapador(int posX,int posY,Animacion image){
+		super(posX,posY,image);	
+	}
+    
+}
---
+++ Parabola/src/parabola/Lanzado.java
@@ -0,0 +1,17 @@
+package parabola;
+
+//Clase para el objeto lanzado
+public class Lanzado extends Base{
+    
+        /**
+	 * Metodo constructor que hereda los atributos de la clase <code>Universo</code>.
+	 * @param posX es la <code>posiscion en x</code> del objeto Planeta.
+	 * @param posY es el <code>posiscion en y</code> del objeto Planeta.
+	 * @param image es la <code>imagen</code> del objeto Planeta.
+	 */
+    
+    public Lanzado(int posX,int posY,Animacion image){
+		super(posX,posY,image);	
+	}
+    
+}
